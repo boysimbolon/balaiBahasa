3d1b6752c310746cc01d42642ab4d5b7d35103d1
Boy Simbolon <2281040@unai.edu>
2024-08-22 14:41:50 +0700

    auth table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('no_Peserta')->unique();
+            $table->string('pin');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
