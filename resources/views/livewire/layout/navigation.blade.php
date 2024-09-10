f3f67fc866295a8f876c5784512a4f814ca86612
yosiaevan <yosiaevan23@gmail.com>
2024-09-10 14:05:23 +0700

    fix navigation and app template

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -29,18 +29,21 @@ new class extends Component
     <ul class="mt-5 h-full flex flex-col justify-between">
         <div>
             <!-- Dashboard -->
-            <li class="flex items-center gap-2 h-14 px-5 bg-secondary text-black">
-                <a href="{{ route('dashboard') }}" class="flex items-center gap-2" wire:navigate>
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="black" class="bi bi-grid" viewBox="0 0 16 16">
-                            <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z"/>
-                        </svg>
+            <a href="{{ route('dashboard') }}">
+                <li class="flex items-center gap-2 h-14 px-5 hover:bg-secondary hover:text-black {{ Route::is('dashboard') ? 'bg-secondary text-black' : 'text-white' }}">
+                    <div class="flex items-center gap-2" wire:navigate>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="black" class="bi bi-grid" viewBox="0 0 16 16">
+                                <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z"/>
+                            </svg>
+                        </div>
+                        <p class="text-lg">Dashboard</p>
                     </div>
-                    <p class="text-lg">Dashboard</p>
-                </a>
-            </li>
+                </li>
+            </a>
+
             <!-- Profile -->
-            <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
+            <li class="flex items-center justify-between h-14 px-5 hover:bg-secondary hover:text-black  {{ Route::is('biodata') ? 'bg-secondary text-black' : 'text-white' }}">
                 <div class="flex items-center gap-2 w-fit">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="black" class="bi bi-person" viewBox="0 0 16 16">
@@ -49,27 +52,34 @@ new class extends Component
                     </div>
                     <p class="text-lg">Profile</p>
                 </div>
-                <svg onclick="openProfileDropdown()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-caret-down-fill" viewBox="0 0 16 16" id="caret1">
+                <svg onclick="openProfileDropdown()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-caret-down-fill hover:cursor-pointer" viewBox="0 0 16 16" id="caret1">
                     <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                 </svg>
             </li>
+
             <!-- Profile Dropdown -->
             <ul class="hidden" id="profileDropdown">
                 <!-- Biodata -->
-                <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
-                    <a class="flex items-center gap-2 w-fit" href="{{ route('biodata') }}">
-                        <div class="w-6"></div>
-                        <p class="text-lg">Biodata</p>
-                    </a>
-                </li>
+                <a href="{{ route('biodata') }}">
+                    <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
+                        <div class="flex items-center gap-2 w-fit">
+                            <div class="w-6"></div>
+                            <p class="text-lg">Biodata</p>
+                        </div>
+                    </li>
+                </a>
+
                 <!-- Change Password -->
-                <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
-                    <div class="flex items-center gap-2 w-fit">
-                        <div class="w-6"></div>
-                        <p class="text-lg">Ganti Password</p>
-                    </div>
-                </li>
+                <a href="">
+                    <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
+                        <div class="flex items-center gap-2 w-fit">
+                            <div class="w-6"></div>
+                            <p class="text-lg">Ganti Password</p>
+                        </div>
+                    </li>
+                </a>
             </ul>
+
             <!-- Type Test -->
             <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
                 <div class="flex items-center gap-2">
@@ -80,40 +90,52 @@ new class extends Component
                     </div>
                     <p class="text-lg text-nowrap">Pilih Jenis Tes</p>
                 </div>
-                <svg onclick="openTypeDropdown()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-caret-down-fill" viewBox="0 0 16 16" id="caret2">
+                <svg onclick="openTypeDropdown()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-caret-down-fill hover:cursor-pointer" viewBox="0 0 16 16" id="caret2">
                     <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                 </svg>
             </li>
+
             <!-- Type Test Dropdown -->
             <ul class="hidden" id="typeDropdown">
                 <!-- E3 -->
-                <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
-                    <div class="flex items-center gap-2 w-fit">
-                        <div class="w-6"></div>
-                        <p class="text-lg">E3</p>
-                    </div>
-                </li>
+                <a href="">
+                    <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
+                        <div class="flex items-center gap-2 w-fit">
+                            <div class="w-6"></div>
+                            <p class="text-lg">E3</p>
+                        </div>
+                    </li>
+                </a>
+
                 <!-- TOEFL -->
-                <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
-                    <div class="flex items-center gap-2 w-fit">
-                        <div class="w-6"></div>
-                        <p class="text-lg">TOEFL</p>
+                <a href="">
+                    <li class="flex items-center justify-between h-14 px-5 text-white hover:bg-secondary hover:text-black">
+                        <div class="flex items-center gap-2 w-fit">
+                            <div class="w-6"></div>
+                            <p class="text-lg">TOEFL</p>
+                        </div>
+                    </li>
+                </a>
+            </ul>
+
+            <!-- History -->
+            <a href="">
+                <li class="flex items-center h-14 px-5 text-white hover:bg-secondary hover:text-black">
+                    <div class="flex items-center gap-2">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="black" class="bi bi-clock" viewBox="0 0 16 16">
+                                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+                                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+                            </svg>
+                        </div>
+                        <p class="text-lg">History</p>
                     </div>
                 </li>
-            </ul>
-            <li class="flex items-center h-14 px-5 text-white hover:bg-secondary hover:text-black">
-                <div class="flex items-center gap-2">
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="black" class="bi bi-clock" viewBox="0 0 16 16">
-                            <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
-                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
-                        </svg>
-                    </div>
-                    <p class="text-lg">History</p>
-                </div>
-            </li>
+            </a>
         </div>
-        <li class="flex items-center h-14 px-5 text-white hover:bg-secondary hover:text-black">
+
+        <!-- Log Out -->
+        <li class="flex items-center h-14 px-5 text-white hover:bg-secondary hover:text-black hover:cursor-pointer" wire:click="logout">
             <div class="flex items-center gap-2 w-fit">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="black" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
