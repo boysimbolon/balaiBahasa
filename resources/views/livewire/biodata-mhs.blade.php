997b89b5fdc6094ac0698d45f168a4a68358c775
yosiaevan <yosiaevan23@gmail.com>
2024-10-03 16:03:26 +0700

    update biodata for mahasiswa

--- resources/views/livewire/biodata-mhs.blade.php
+++ resources/views/livewire/biodata-mhs.blade.php
@@ -1,148 +1,80 @@
 @section('title', $title)
-<div>
-    <x-app-layout>
-        <div class="p-5 h-full">
-            <x-heading-page>Profil</x-heading-page>
-            <div class="flex flex-col xl:flex-row gap-5">
-                <div class="bg-white drop-shadow-lg w-full order-2 xl:order-1 xl:w-2/3 mt-1 rounded p-5 flex flex-col gap-4">
-                    <div class="divide-y-2">
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Tempat Lahir</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
-                                {{$data['temp_lahir']}}
-                            </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Tanggal Lahir</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
-                                {{$data['tgl_lahir']}}
-                            </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Jenis Kelamin</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
+<x-app-layout>
+    <h4 class="page-title text-2xl font-medium">Profil</h4>
+    <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-4">
+        <div class="lg:order-1">
+            <div class="card text-center">
+                <div class="card-body">
+                    <img src="{{ 'https://online.unai.edu/mhs/'.$foto }}" class="bg-light w-100 h-100 rounded-full mx-auto avatar-lg img-thumbnail" alt="profile-image">
+
+                    <h4 class="mb-0 mt-2 text-2xl font-medium">{{ $data['nama'] }}</h4>
+                    <p class="text-muted text-base my-10">({{ trim($data['nim']) }})</p>
+
+                    <div class="text-start mt-3">
+                        <p class="text-muted mb-2 "><strong class="text-info">Telepon :</strong><span class="ms-2">{{ $data['notelp'] }}</span></p>
+                        <p class="text-muted mb-2 "><strong class="text-info">Email :</strong> <span class="ms-2 ">{{ $data['email'] }}</span></p>
+                        <p class="text-muted mb-1 "><strong class="text-info">Kewarganegaraan :</strong> <span class="ms-2">Indonesia</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-2">
+            <div class="box">
+                <div class="box-body p-0">
+                    <div class="media-list media-list-hover media-list-divided">
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Tempat Lahir</span>
+                            <span>{{ $data['temp_lahir'] }}</span>
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Tanggal Lahir</span>
+                            <span>{{ $data['tgl_lahir'] }}</span>
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Jenis Kelamin</span>
+                            <span>
                                 @if($data['sex'] == 'L')
                                     Laki-laki
-                                    @else
+                                @else
                                     Perempuan
                                 @endif
                             </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Alamat</span>
-                                <span class="w-1/2 md:w-fit flex items-center gap-5 text-right md:text-left text-wrap">
-                                <span class="hidden md:block">:</span>
-                                {{$alamat}}
-                            </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Pekerjaan</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
-                                Mahasiswa
-                            </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">NIDN</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
-                            </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Instansi</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
-                                Universitas Advent Indonesia
-                            </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Pendidikan</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
-                                S1
-                            </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Tahun Pendidikan</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
-                                2024
-                            </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Kewarganegaraan</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
-                                Indonesia
-                            </span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">
-                                <span class="w-1/2 md:w-1/5 font-semibold">Bahasa</span>
-                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">
-                                <span class="hidden md:block">:</span>
-                                Indonesia
-                            </span>
-                            </p>
-                        </div>
-{{--                        <div class="min-h-11 max-h-fit flex items-center py-3">--}}
-{{--                            <p class="w-full flex items-center gap-10 text-nowrap justify-between md:justify-normal">--}}
-{{--                                <span class="w-1/2 md:w-1/5 font-semibold">KTP</span>--}}
-{{--                                <span class="w-1/2 flex items-center gap-5 justify-end md:justify-normal">--}}
-{{--                                <span class="hidden md:block">:</span>--}}
-{{--                                <img src="https://picsum.photos/300/200" alt="" class="rounded-xl">--}}
-{{--                            </span>--}}
-{{--                            </p>--}}
-{{--                        </div>--}}
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Alamat</span>
+                            <span>{{ $alamat }}</span>
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Pekerjaan</span>
+                            <span>Mahasiswa</span>
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">NIDN</span>
+                            <span></span>
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Instansi</span>
+                            <span>Universitas Advent Indonesia</span>
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Pendidikan</span>
+                            <span>S1</span>
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Tahun Pendidikan Pendidikan</span>
+                            <span>2024</span>
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Bahasa</span>
+                            <span>Indonesia</span>
+                        </a>
+                        <a class="media media-single" href="#">
+                            <span class="title text-mute">Bahasa</span>
+                            <span>Indonesia</span>
+                        </a>
                     </div>
                 </div>
-                <div class="w-full order-1 xl:order-2 xl:w-1/3">
-                    <div class="bg-white drop-shadow-lg h-fit mt-1 rounded p-5">
-                        <div class="flex items-center flex-col gap-4 mb-5">
-                            <img src="{{'https://online.unai.edu/mhs/'.$foto}}" alt="" width="200" height="300" class="rounded-xl">
-                            <div class="text-center">
-                                <h1 class="text-xl font-bold">{{$data['nama']}}</h1>
-                                <p class="text-center">{{$data['nim']}}</p>
-                            </div>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center">
-                            <p class="w-full flex items-center justify-between text-nowrap">
-                                <span class="font-semibold">Telepon</span>
-                                <span>{{$data['notelp']}}</span>
-                            </p>
-                        </div>
-                        <div class="min-h-11 max-h-fit flex items-center w-full">
-                            <p class="w-full flex items-center justify-between text-nowrap">
-                                <span class="font-semibold">Email</span>
-                                <span class="text-wrap">{{$data['email']}}</span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-             </div>
-        </div>
-    </x-app-layout>
-</div>
+            </div>
+        </div> <!-- end col -->
+    </div>
+</x-app-layout>
