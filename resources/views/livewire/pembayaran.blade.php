6f768bd6d725b42971bad858459a46f496d42997
yosiaevan <yosiaevan23@gmail.com>
2024-10-04 09:56:41 +0700

    fix: improve payment history UI for students

--- resources/views/livewire/pembayaran.blade.php
+++ resources/views/livewire/pembayaran.blade.php
@@ -12,17 +12,50 @@
                 <div class="box-body">
                     <p class="mb-4">Untuk melanjutkan proses pendaftaran ujian, Anda diwajibkan untuk melakukan pembayaran Ujian. Pembayaran dapat dilakukan melalui BNI Virtual Account.</p>
                     <div class="flex justify-between items-center mt-3">
-                        <h2 class="box-title text-xl m-0">Nomor Virtual Account : xxxxxxxxxxxxxxxx</h2>
+                        <h2 class="box-title text-xl m-0 font-bold">Nomor Virtual Account : xxxxxxxxxxxxxxxx</h2>
                     </div>
                 </div>
             </div>
         </div>
-        <div>
+        <div class="">
             <div class="box">
                 <div class="box-body">
-                    <p class="mb-4">Untuk melanjutkan proses pendaftaran ujian, Anda diwajibkan untuk melakukan pembayaran Ujian. Pembayaran dapat dilakukan melalui BNI Virtual Account.</p>
-                    <div class="flex justify-between items-center mt-3">
-                        <h2 class="box-title text-xl m-0">Nomor Virtual Account : xxxxxxxxxxxxxxxx</h2>
+                    <h4 class="box-title text-xl font-medium">History Pembayaran</h4>
+                    <div class="table-responsive">
+                        <table class="text-fade table w-full ">
+                            <thead class="bg-primary text-left">
+                            <tr>
+                                <th>Jenis Ujian</th>
+                                <th>Tanggal Ujian</th>
+                                <th>Jam</th>
+                                <th>Lokasi</th>
+                                <th>Waktu Pesan</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($Data->sortBy([['listujian.tipeujian.jenis_ujian', 'asc'], ['listujian.tanggal', 'asc'],['listujian.jam','asc']]) as $psn => $data)
+                                @if(isset($data->listujian->id_jenis_ujian))
+                                    <tr class="border-y">
+                                        <td class="p-2">{{ $data->listujian->tipeujian->jenis_ujian }}</td>
+                                        <td class="p-2">{{ $tgl[$psn] ?? 'N/A' }}</td>
+                                        <td class="p-2">{{ $jm[$psn] ?? 'N/A' }}</td>
+                                        <td class="p-2">{{ $data->listruangan->nama_ruangan }}</td>
+                                        <td class="p-2">{{ $created[$psn] ?? 'N/A' }}</td>
+                                        <td class="p-2">
+                                            @if(!$data->status)
+                                                <button class="bg-primary text-white py-2 px-4 rounded"
+                                                        wire:click="Cek({{ $data }})"
+                                                        wire:confirm="Apakah Anda sudah melakukan Pembayaran?">Bayar</button>
+                                            @else
+                                                Berhasil
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endif
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -43,26 +76,7 @@
 {{--                        <th class="p-2">Waktu Pesan</th>--}}
 {{--                        <th class="p-2"></th>--}}
 {{--                    </tr>--}}
-{{--                    @foreach($Data->sortBy([['listujian.tipeujian.jenis_ujian', 'asc'], ['listujian.tanggal', 'asc'],['listujian.jam','asc']]) as $psn => $data)--}}
-{{--                        @if(isset($data->listujian->id_jenis_ujian))--}}
-{{--                            <tr class="border-y">--}}
-{{--                                <td class="p-2">{{ $data->listujian->tipeujian->jenis_ujian }}</td>--}}
-{{--                                <td class="p-2">{{ $tgl[$psn] ?? 'N/A' }}</td>--}}
-{{--                                <td class="p-2">{{ $jm[$psn] ?? 'N/A' }}</td>--}}
-{{--                                <td class="p-2">{{ $data->listruangan->nama_ruangan }}</td>--}}
-{{--                                <td class="p-2">{{ $created[$psn] ?? 'N/A' }}</td>--}}
-{{--                                <td class="p-2">--}}
-{{--                                    @if(!$data->status)--}}
-{{--                                        <button class="bg-primary text-white py-2 px-4 rounded"--}}
-{{--                                                wire:click="Cek({{ $data }})"--}}
-{{--                                                wire:confirm="Apakah Anda sudah melakukan Pembayaran?">Bayar</button>--}}
-{{--                                    @else--}}
-{{--                                        Berhasil--}}
-{{--                                    @endif--}}
-{{--                                </td>--}}
-{{--                            </tr>--}}
-{{--                        @endif--}}
-{{--                    @endforeach--}}
+
 {{--                </table>--}}
 {{--            </div>--}}
 {{--        </div>--}}
